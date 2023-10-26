fb821d30f4dd24c989389b30eea524f85d9df865
theof <the>
2023-10-26 11:38:13 +0200

    update

---
+++ src/assets/datas/index.js
@@ -0,0 +1,10 @@
+export const coordinates = [
+    {lat: 47.203217,lng: -1.535365,name: "J. Sparrow",},
+    {lat: 47.206264,lng: -1.545764,name: "L. Kennedy",},
+    {lat: 47.203126,lng: -1.550022,name: "A. Skywalker",},
+    {lat: 47.208558,lng: -1.535796,name: "H. Potter",},
+    {lat: 47.223825,lng: -1.557663,name: "A. Tano",},
+    {lat: 47.206209,lng: -1.514515,name: "H. Granger",},
+    {lat: 47.240354,lng: -1.639656,name: "E. Swan",},
+];
+//47.240354
--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,9 +1,10 @@
 import {IonCol, IonContent, IonPage, IonRow, IonSearchbar, useIonViewWillEnter} from '@ionic/react';
-import React, {useRef, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import './Map.css';
 import Header from "../components/header/header";
 
-import {GoogleMap} from '@capacitor/google-maps';
+import {Circle, GoogleMap} from '@capacitor/google-maps';
+import {coordinates} from '../assets/datas';
 
 
 const Map: React.FC = () => {
@@ -13,6 +14,13 @@ const Map: React.FC = () => {
     const key = "AIzaSyBGh2dsWZifqV6MH4JrhJKHmobIwT0fBfo";
     const mapRef = useRef<HTMLElement>();
     let newMap: GoogleMap;
+    const radius = 2500;
+    const circleCenter = {
+        lat: 47.206215,
+        lng: -1.539279,
+    };
+
+
 
     async function createMap() {
         if (!mapRef.current) return;
@@ -22,33 +30,50 @@ const Map: React.FC = () => {
             element: mapRef.current,
             apiKey: key,
             config: {
-                center: {
-                    lat: 47.206215,
-                    lng: -1.539279
-                },
+                center: circleCenter,
                 zoom: 12
             }
-        })
+        });
+        addCircleToMap(newMap);
     }
-    /*let newMap;
-    const mapRef = useRef(null);
-    const [mapConfig, setMapConfig] = useState({
-        zoom: 12,
-        center: {
-            lat: 47.206527,
-            lng: -1.539279
-        }
+    function addCircleToMap(map: GoogleMap) {
+        const circleOptions = [{
+            center: {
+                lat: 47.206215,
+                lng: -1.539279,
+            },
+            radius: radius, // Radius in meters
+            strokeColor: '#FF0000',
+            strokeWidth: 2,
+            fillColor: '#FF000080', // Circle fill color with alpha (transparency)
+        }];
+
+        map.addCircles(circleOptions);
+    }
+    const results = coordinates.map((coord) => {
+        const lat2 = coord.lat;
+        const lng2 = coord.lng;
+
+        const R = 6371; // Radius of the Earth in kilometers
+        const lat1 = circleCenter.lat;
+        const lng1 = circleCenter.lng;
+
+        const dLat = (lat2 - lat1) * (Math.PI / 180);
+        const dLng = (lng2 - lng1) * (Math.PI / 180);
+
+        const a =
+            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(lat1 * (Math.PI / 180)) * Math.cos(lat2 * (Math.PI / 180)) * Math.sin(dLng / 2) * Math.sin(dLng / 2);
+
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        const distance = R * c;
+
+        return distance <= radius;
     });
-    const createMap = async () => {
-        if (!mapRef.current) return;
-        newMap = await GoogleMap.create({
-            id: "google-map",
-            element: mapRef.current,
-            apiKey: key,
-            config: mapConfig,
-        })
-    }*/
-    useIonViewWillEnter(() => createMap());
+    useEffect(() => {
+        createMap();
+    }, []);
+0
     return (
         <IonPage>
             <Header/>
@@ -65,7 +90,18 @@ const Map: React.FC = () => {
                             width: "100%",
                             height: 400
                         }}/>
-
+                </IonCol>
+            </IonRow>
+            <IonRow>
+                <IonCol>
+                    <div>
+                        <h2>Locations Inside the Circle:</h2>
+                        <ul>
+                            {results.map((isInside, index) => (
+                                isInside && <li key={index}>{coordinates[index].name}</li>
+                            ))}
+                        </ul>
+                    </div>
                 </IonCol>
             </IonRow>
         </IonContent>
